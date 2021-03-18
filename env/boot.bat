e8c81caadbd515c5c776fa4939e483c348377139
hatena75 <hatena75@outlook.jp>
2021-03-19 00:06:39 +0900

    QEMUの起動コマンド設定

---
+++ env/boot.bat
@@ -0,0 +1,1 @@
+@qemu-system-i386.exe -L "C:\Program Files\qemu" -rtc base=localtime -drive file=C:\prog\src\00_boot_only\boot.img,format=raw -boot order=c
--- env/env.bat
+++ env/env.bat
@@ -1,2 +1,3 @@
 @set PATH=%PATH%;..\..\env
 @set PATH=%PATH%;..\..\tools\nasm-2.14.02
+@set PATH=%PATH%;"C:\Program Files\qemu"
